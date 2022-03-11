16ed8ed9b5ef5714c495a16145663c38046886d5
David Puerto Guerrero <dsp5502@gmail.com>
2022-03-11 18:57:29 -0500

    showdata

--- Workshop1/scripts/showdata.js
+++ Workshop1/scripts/showdata.js
@@ -1,10 +1,10 @@
 const showData = async (arrProductos, cards) => {
   const productos = await arrProductos;
   productos.forEach((product) => {
-    const { name, precio, imagen, descripcion } = product;
+    const { name, precio, imagen, descripcion, id } = product;
     cards.innerHTML += `
     <div> ${name} ${precio} 
-    <img src="${imagen}" alt="imagen">
+     <a href="" id="${id}"><img src="${imagen}" alt="imagen"></a>
     </div>
     `;
   });
