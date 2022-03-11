2e138c1107b9aeae0e9413b5b31c9ac6bd28a19c
osccso <osm428@hotmail.com>
2022-03-11 16:55:29 -0500

    agregado DOMcontentload

--- Workshop1/scripts/main.js
+++ Workshop1/scripts/main.js
@@ -1,1 +1,10 @@
+import getData from "./getdata.js"
 
+const urlproductos =  "https://ejemplo-backend.herokuapp.com/productos"
+
+
+document.addEventListener("DOMContentLoaded",async () => {
+    const datos = await getData(urlproductos)
+    console.log(datos)
+}
+)
