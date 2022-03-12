8a954d7f9e1dac9f5f3f91747384439fda3a303b
Hipns1 <jesudpf21@hotmail.com>
2022-03-12 01:23:43 -0500

    Proyecto Finalizado

--- Workshop1/scripts/main.js
+++ Workshop1/scripts/main.js
@@ -39,7 +39,50 @@ const mostrarProducto = async (producto, card) => {
             <a><img class="btnimagen" src="${imagen[2]}" alt="imagen"></a>
         </div>
         <div class="imgPrincipalDetail">
-            <a><img class="btnimagen" src="${imagen[0]}" alt="imagen"></a>
+            <div id="carouselExampleIndicators" class="carousel slide" data-mdb-ride="carousel">
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-mdb-target="#carouselExampleIndicators"
+                  data-mdb-slide-to="0"
+                  class="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-mdb-target="#carouselExampleIndicators"
+                  data-mdb-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-mdb-target="#carouselExampleIndicators"
+                  data-mdb-slide-to="2"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="${imagen[0]}" class="d-block w-100" alt="Wild Landscape"/>
+                </div>
+                <div class="carousel-item">
+                  <img src="${imagen[1]}" class="d-block w-100" alt="Camera"/>
+                </div>
+                <div class="carousel-item">
+                  <img src="${imagen[2]}" class="d-block w-100" alt="Exotic Fruits"/>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleIndicators" data-mdb-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+
         </div>
         <div class="textDetail">
             <h1>${name}</h1>
