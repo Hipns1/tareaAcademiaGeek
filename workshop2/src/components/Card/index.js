61b0e23018cdd287e9a1d654496938ad1448c7f8
Yessith Estiven Herrera Sanjuan <yessith.hs@gmail.com>
2022-03-31 15:06:28 -0500

    Add card components

---
+++ workshop2/src/components/Card/index.js
@@ -0,0 +1,25 @@
+export function Card() {
+	return (
+		<article>
+			<span>⭐</span>
+			<div>
+				<h2>Chief Operations Officer</h2>
+				<p>International</p>
+			</div>
+			<div>
+				<span>Robin Price</span>
+				<span>Tomas Mosciski Sr.</span>
+				<span>Karl Torp</span>
+			</div>
+			<div>
+				<h3>Industry Segment: Creative</h3>
+				<h3>primary_topic: Garden</h3>
+				<h3>session_type:Strategist</h3>
+				<h3>audience_type:Trans*Female</h3>
+				<h3>lenguage:KW</h3>
+				<h3>audience_level:Assistant</h3>
+				<h3>date:2022-06-06</h3>
+			</div>
+		</article>
+	)
+}
