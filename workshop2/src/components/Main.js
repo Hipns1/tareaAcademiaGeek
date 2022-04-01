b1de5d36642597a7b5773cd75c9ddc544136259f
Hipns1 <jesudpf21@hotmail.com>
2022-03-31 19:14:01 -0500

    push

--- workshop2/src/components/Main.js
+++ workshop2/src/components/Main.js
@@ -4,21 +4,7 @@ import { starCard } from "../helpers/starCard";
 import { url } from "../helpers/url";
 
 const Main = () => {
-  /* const [data, setData] = useState([]);
-  useEffect(() => {
-    getData(url).then((data) => {
-      setData(data);
-    });
-  }, []);
-
-  useEffect(() => {
-    starCard(data[2]);
-    console.log(localStorage.getItem("starred"));
-
-    console.log(data);
-  }, [data]); */
-
-  return <div>Main</div>;
+return <div>Main</div>;
 };
 
 export default Main;
